e172b01f4cf2a9201f083657174b14237c66f1e2
ajalvarez89 <alvaro.alvarez1089@gmail.com>
2017-04-22 18:00:53 -0500

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,5 +1,5 @@
 <?php
-define('DB_HOST','us-cdbr-iron-east-03.cleardb.net');
+define('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net');
 define('DB_USER', 'b1a730213439ba');
 define('DB_PASS', 'b88a3cdde693d24');
 define('DATABASE', 'heroku_150c39512e84003');
