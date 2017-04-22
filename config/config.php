aa3414007cf03df40de2fdf65957ae7c9dedae78
ajalvarez89 <alvaro.alvarez1089@gmail.com>
2017-04-22 16:44:13 -0500

    Update config.php
    modificacion login heroku

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
-define('DB_HOST','localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DATABASE', 'udemy');
+define('DB_HOST','us-cdbr-iron-east-03.cleardb.net');
+define('DB_USER', 'b1a730213439ba');
+define('DB_PASS', 'b88a3cdde693d24');
+define('DATABASE', 'heroku_150c39512e84003');
 ?>
 
